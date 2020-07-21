158c0a2d074be88ea12ec1d30d53906c130edde3
Tabitha Princess Uwimpuhwe <tabithauwimpuhwe@gmail.com>
2020-07-21 13:49:00 -0700

    search-filter function started

--- revatureproject/src/app/todos-list/todos-list/todos-list.component.ts
+++ revatureproject/src/app/todos-list/todos-list/todos-list.component.ts
@@ -12,9 +12,38 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class TodosListComponent implements OnInit {
 
+  filteredTodos: ITodos[];
+  attrListFilter = 'Search the Todos List';
+
   todos: ITodos[];
 
-  constructor(private todoServ: TodosService) { }
+
+  constructor(private route: ActivatedRoute, private todoServ: TodosService) { }
+
+  get listFilter(): string {
+    return this.attrListFilter;
+  }
+
+  set listFilter(temp: string) {
+    this.attrListFilter = temp;
+    this.filteredTodos = this.attrListFilter ?
+      this.performFilter(this.attrListFilter) : this.todos;
+  }
+
+  performFilter(filterBy: string): ITodos[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    this.todoServ.getTodos().subscribe(
+      todos => {
+        console.log('Lenght ' + todos.length);
+       return  this.todos.filter((myTodoslist: ITodos) =>
+        myTodoslist.title.toLocaleLowerCase().indexOf(filterBy) !== -1);
+        console.log('Something 1 ' + todos.filter);
+        });
+      //  return this.todos.filter((myTodoslist: ITodos)) =>
+    console.log('Something 2 ' + this.todos);
+    return this.todos;
+
+  }
 
     
     getTodosEc2() {
@@ -33,11 +62,8 @@ export class TodosListComponent implements OnInit {
           });
     }
       
-    
-
-   
-
   ngOnInit(): void {
+    // this.currentHero = this.route.snapshot.paramMap.get('heroname');
   }
 
 }
